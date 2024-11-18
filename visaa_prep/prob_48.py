173a34f8bd33d5de93413c60e96bce672bb74fbf
Sravani <142093684+Sravani722@users.noreply.github.com>
2024-11-18 14:34:16 +0530

    Create prob_48.py

---
+++ visaa_prep/prob_48.py
@@ -0,0 +1,12 @@
+def plusMinus(arr):
+    a,b,c=0,0,0
+    for i in arr:
+        if i>0:
+            a+=1
+        elif i<0:
+            b+=1
+        else:
+            c+=1
+    print(a/n)
+    print(b/n)
+    print(c/n)
